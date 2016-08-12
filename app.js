a6567679cd4a90f44dd0899624f08a839187f5b4
Ubuntu <ubuntu@ip-172-31-20-43.ap-northeast-1.compute.internal>
2016-08-12 08:14:57 +0000

    add modules

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-var express =
+var express = require('express');
+var mongoClient = require('mongodb');
+
+var app = express();
+var url = 'mongodb://localhost:27017/ServerMonitor';
+
+app.get('/Get/:ip', function (req, res) {
+
+	
+});
+
