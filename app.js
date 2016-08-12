e7e8b4be9d78d703668634052bbe058f11765b7d
Ubuntu <ubuntu@ip-172-31-20-43.ap-northeast-1.compute.internal>
2016-08-12 10:25:37 +0000

    query mongodb

--- app.js
+++ app.js
@@ -7,10 +7,33 @@ var app = express();
 var eventEmitter = new events.EventEmitter();
 var url = 'mongodb://localhost:27017/ServerMonitor';
 
+var query = function (ip, date, callback) {
+	mongoClient.connect(url, function (err, db) {
+		var cursor = db.collection('ping').find({ "ip":ip, "date":date });
+
+		var results = new Array();
+		cursor.each(function (err, doc) {
+			if (err != null) { console.log(err); }
+			if (doc != null) {
+				results.push(doc);
+			} else {
+				db.close();
+				callback(results);
+			}
+		});
+	});
+}
 
 app.get('/api/:ip/:date', function (req, res) {
 	ip = req.params.ip;
 	date = req.params.date
 	
+	query(ip, date, function (data) {
+		if (data != null) {
+			res.end(data);
+		} else {
+			res.end("no data");
+		}
+	});
 });
 
