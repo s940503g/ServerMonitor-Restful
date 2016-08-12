73ad91043d3e414cd9723c0431a5cfd32f866316
Ubuntu <ubuntu@ip-172-31-20-43.ap-northeast-1.compute.internal>
2016-08-12 09:51:17 +0000

    app.get function

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 var express = require('express');
-var mongoClient = require('mongodb');
+var mongoClient = require('mongodb').MongoClient;
+var events = require('events');
+
 
 var app = express();
+var eventEmitter = new events.EventEmitter();
 var url = 'mongodb://localhost:27017/ServerMonitor';
 
-app.get('/Get/:ip', function (req, res) {
 
+app.get('/api/:ip/:date', function (req, res) {
+	ip = req.params.ip;
+	date = req.params.date
 	
 });
 
