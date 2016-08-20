26a5a4286eb9bd10714ce12823e7103182cc6780
Ubuntu <ubuntu@ip-172-31-20-43.ap-northeast-1.compute.internal>
2016-08-20 06:26:15 +0000

    simple restful function finished

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var url = 'mongodb://localhost:27017/ServerMonitor';
 
 var query = function (ip, date, callback) {
 	mongoClient.connect(url, function (err, db) {
-		var cursor = db.collection('TEST').find({ "ip":ip, "date":date });
+		var cursor = db.collection('TEST').find({ 'ip':ip, 'date':date });
 
 		var results = new Array();
 		cursor.each(function (err, doc) {
@@ -18,6 +18,7 @@ var query = function (ip, date, callback) {
 				results.push(doc);
 			} else {
 				db.close();
+//				callback(doc);
 				callback(results);
 			}
 		});
@@ -25,18 +26,26 @@ var query = function (ip, date, callback) {
 }
 
 app.get('/api/:ip/:date', function (req, res) {
-	ip = req.params.ip;
-	date = req.params.date
-	
-	query(ip, date, function (data) {
+	var ip = req.params.ip; // e.g. 52-32-63-252
+	var date = req.params.date; // e.g. 2016-08-20
+
+
+	console.log("query by "+ipFormate(ip)+" "+date);	
+
+	query(ipFormate(ip), date, function (data) {
 		if (data != null) {
-			res.end(data);
+			res.end(JSON.stringify(data));
 		} else {
 			res.end("no data");
 		}
 	});
+
 });
 
-app.listen(8081, function () {
-	console.log("ready port for 8081");
+function ipFormate (ip) {
+	ip = ip.replace(/-/g, '.');
+	return ip;
+}
+app.listen(8080, function () {
+	console.log("ready port for 8080");
 });
